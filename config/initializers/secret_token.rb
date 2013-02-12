a11b9dfdcd82b58fe710c0734889c7d1536fc0da
Mohammed Al-Sheikh <msheikh2009@yahoo.com>
2013-02-12 09:40:43 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maktabti::Application.config.secret_token = 'fe8be731258ddf4ee973bda94ca1bec88aff4a4d171e1e0d2365082798cfc002ddc369668fcc02cbbe826ba389264227e39b247d7846a019e541f512a9ff0642'
